0a3231303788b3b3a210fd3f0e8594cf915ba222
Dylan Nelson <nlsdyl001@myuct.ac.za>
2021-08-21 05:27:43 +0200

    multiplicative Z/nZ

--- z_mod_nz_experiment.py
+++ z_mod_nz_experiment.py
@@ -7,5 +7,21 @@ output_parameterised_experiment(
 	2,
 	100,
 	None,
-	1000
+	10000
+)
+
+gcd = lambda a, b: a if b == 0 else gcd(b, a % b)
+
+print("""
+
+Multiplicative
+""")
+output_parameterised_experiment(
+	lambda n: [k for k in range(n) if gcd(k, n) == 1],
+	lambda n: lambda x, y: (x * y) % n,
+	lambda n: 1,
+	2,
+	100,
+	None,
+	10000
 )
